dee1952a237489c99d46da106597c66ea5ca9679
root <root@ip-172-31-27-118.ec2.internal>
2020-11-10 20:41:40 +0000

    app.js amended

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">No items yet! Add one above abovvvv!</p>
+                <p className="text-center">Hadi bakalim !</p>
             )}
             {items.map(item => (
                 <ItemDisplay
